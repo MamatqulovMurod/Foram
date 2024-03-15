7458b728ed2a5d5dff01c4f97f6c2bbe102da654
macuser <mamatqulovmurod383@gmail.com>
2024-03-15 02:02:31 +0000

    INFRA: Initialize API Project

--- Foram.Api/Program.cs
+++ Foram.Api/Program.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+//Copyright (c) Coalition of Good-Hearted Engiineers
+//Free to Use Comfort and Peace
+//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Foram.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                        webBuilder.UseStartup<Startup>());
+
+        }
     }
 }
 
--- Foram.Api/Startup.cs
+++ Foram.Api/Startup.cs
@@ -1,59 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+//Copyright (c) Coalition of Good-Hearted Engiineers
+//Free to Use Comfort and Peace
+//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Foram.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) =>
+             Configuration = configuration;
+        
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            var apiInfo = new OpenApiInfo
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Foram.Api", Version = "v1" });
+                Title = "Foram.Api",
+                Version = "V1"
+            };
+            services.AddControllers();
+
+            services.AddSwaggerGen(options=>
+            {
+                options.SwaggerDoc(
+                    name: "v1",
+                    info: apiInfo);
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment environment)
         {
-            if (env.IsDevelopment())
+            if (environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foram.Api v1"));
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json",
+                        name: "Foram.Api v1");
+
+                });
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+                endpoints.MapControllers());
+            
         }
     }
 }
