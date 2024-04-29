92d8f176a629193b6545314254e8e70c8f35599b
MamatqulovMurod <mamatqulovmurod383@gmail.com>
2024-04-29 09:59:39 +0500

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ Foram.Api/Models/Foundations/Guests/Exceptions/FailedGuestServiceException.cs
@@ -0,0 +1,19 @@
+﻿//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+//Copyright (c) Coalition of Good-Hearted Engiineers
+//Free to Use To Find Comfort and Peace
+//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using System;
+using Xeptions;
+
+namespace Foram.Api.Models.Foundations.Guests.Exceptions
+{
+    public class FailedGuestServiceException: Xeption 
+    {
+        public FailedGuestServiceException(Exception innerException)
+            :base(message:"Failed guest service error occured, contanct support",
+                 innerException)
+        { }
+    }
+
+}
---
+++ Foram.Api/Models/Foundations/Guests/Exceptions/GuestServiceException.cs
@@ -0,0 +1,17 @@
+﻿//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+//Copyright (c) Coalition of Good-Hearted Engiineers
+//Free to Use To Find Comfort and Peace
+//= = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using Xeptions;
+
+namespace Foram.Api.Models.Foundations.Guests.Exceptions
+{
+    public class GuestServiceException: Xeption
+    {
+        public GuestServiceException(Xeption innerException)
+            : base(message: "Guest service error occured, contact support",
+                 innerException)
+        { }
+    }
+}
