2f81dbcfb108e4d032f4e3c7b3b4f533b19aa06b
MamatqulovMurod <mamatqulovmurod383@gmail.com>
2024-04-29 15:55:30 +0500

    CONFIG: Register Guest Service

--- Foram.Api/Program.cs
+++ Foram.Api/Program.cs
@@ -10,10 +10,10 @@ namespace Foram.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
-           {
+        public static void Main(string[] args)=>
+           
             CreateHostBuilder(args).Build().Run();
-           }
+           
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
